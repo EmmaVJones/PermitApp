e22437edbd62527982f1783c987d736ce7bc08bd
Emma Jones <evjones@localhost.localdomain>
2017-04-21 16:17:44 -0400

    working gage select and table view

--- server.R
+++ server.R
@@ -2,41 +2,65 @@ source('global.R')
 
 
 shinyServer(function(input, output, session) {
+  activeDot <- function(map,x,y){addCircleMarkers(map,x,y,radius=6,color='blue',fillColor = 'yellow',
+                                                  fillOpacity = 1,opacity=1,weight = 2,stroke=T,layerId = 'Selected')}
   
+  ## Map ## Went with leaflet in the end over mapview
   output$GageMap <- renderLeaflet({
     leaflet()%>%addProviderTiles('Thunderforest.Landscape')%>%addMouseCoordinates()%>%
       addHomeButton(extent(gageInfo), "Virginia Gages")%>%
-      addMarkers(data=gageInfo,layerId=nrow(gageInfo),
+      addCircleMarkers(data=gageInfo,radius=6,color=~'blue',stroke=F,
+                      fillOpacity=0.5,group='gages',layerId=~GageNo,
                  popup=popupTable(gageInfo, zcol = c("GageNo","StationName","DrainArea",
                                                      "HUC8","WebAddress")))
   })
   #output$GageMap <- renderMapview({
     #mapview(gageInfo, popup = popupTable(gageInfo, zcol = c("GageNo","StationName",
-    #                                                        "DrainArea","HUC8","WebAddress")               
-    #                                     ))})
+    #                                                        "DrainArea","HUC8","WebAddress")))})
   
-  # dir
-  shinyDirChoose(input, 'dir', roots = c(home = '~'), filetypes = c('', 'txt'))
-  dir <- reactive(input$dir)
-  output$dir <- renderPrint(dir())
   
-  # path
-  path <- reactive({
-    home <- normalizePath("~")
-    file.path(home, paste(unlist(dir()$path[-1]), collapse = .Platform$file.sep))
-  })
-  
-  # files
-  output$files <- renderPrint(list.files(path()))
-  
-  eventReactive(input$submitZoom,{
+  ## Zoom Button ## broken for now 
+  eventReactive(input$zoomButton,{
     #zoompoint <- 'userpoint'
     #coordinates(zoompoint) <- ~input$long+input$lat
     #proj4string(zoompoint) <- CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")                                 
     #mapview(zoompoint)
     leafletProxy({'GageMap'})%>%setView(input$long,input$lat,zoom=5)
-    
   })
+  
+  
+  ## Move map view to adjust with marker click ##
+  observeEvent(input$GageMap_marker_click,{
+    click <- input$GageMap_marker_click
+    proxy <- leafletProxy("GageMap")
+    if(click$id=="Selected"){
+      proxy%>%removeMarker(layerId='Selected')
+    }else{
+      proxy %>% setView(lng=click$lng,
+                        lat=ifelse(input$GageMap_zoom<10,click$lat+(3/input$GageMap_zoom),click$lat),
+                        input$GageMap_zoom)%>%
+        activeDot(click$lng,click$lat)
+    }
+  })
+    
+  
+  ## Populate table on gage click ##
+  observeEvent(input$GageMap_marker_click,{
+    click<-input$GageMap_marker_click
+    if(!is.null(click$id))
+      output$gageText <- renderPrint({paste('Gage Number:',click$id)})
+  })
+  
+  output$gageInfoTable <- renderTable({
+    if(is.null(input$gageList))
+      return(NULL)
+    d <- subset(gageInfo@data,GageNo %in% input$gageList)
+    names(d) <- c("Gage Number","Station Name","HUC8","Drainage Area","name2","WebAddress") 
+    return(d[1:4,1:4])
+  })
+  
+  
+  
   
 })
 
--- ui.R
+++ ui.R
@@ -4,27 +4,41 @@ shinyUI(fluidPage(theme = "yeti.css", #sandstone #slate good until final DT outp
                     singleton(tags$head(tags$link(href='//cdn.datatables.net/fixedheader/2.1.2/css/dataTables.fixedHeader.css',rel='stylesheet',type='text/css')))
                   ), 
                   navbarPage('VDEQ Permit Tool',
-                             tabPanel('About',fluidRow(column(10,
-                                                              h5("This tool was created to assist VDEQ  staff in the permit review process."),
-                                                              p("Other stuff.")))),
-                             tabPanel("Tab1",
+                              tabPanel("Tab1",
                                       bootstrapPage(div(class="outer",
                                                         tags$style(type ="text/css",".outer {position: fixed; top: 75px; left: 0; right: 0; bottom: 0; overflow: hidden; padding: 0}"),
                                                         leafletOutput("GageMap"),#, width="75%", height="100%"),
                                                         #absolutePanel(top=20, left=70, textInput("target_zoom", "" , "Latitude, Longitude")),
-                                                        h3('Instructions:'),
-                                                        p('Detailed instructions.'),
-                                                        shinyDirButton("dir", "Chose directory", "Upload Shapefiles from Directory"),
-                                                        #fileInput('watershedShapefile',"Input Watershed (shapefile)",
-                                                        #          accept=c('.shp','.dbf','.sbn','.sbx','.shx','.prj'),
-                                                        #          multiple = T),
-                                                        actionButton('zoomButton',"Zoom to Lat/Long",class='btn-block'),
-                                                        verbatimTextOutput('submitZoom')))),
-                             bsModal('zoom',"Zoom to Specific Latitude & Longitude",'zoomButton',size='small',
-                                     numericInput('lat',"Latitude",value=NA),
-                                     numericInput('long',"Longitude",value = NA),
-                                     actionButton('submitZoom','Zoom')),
-                             tabPanel("Tab2")
+                                                        h5('Instructions:'),
+                                                        p('Review the map of current USGS gages for comparison to your watershed. You can zoom to a specific site or
+                                                          simply scroll with your mouse. Clicking on a gage will bring up additional attributes and a link to the NWIS
+                                                          site to review current flow data.'),
+                                                        p('Choose up to four gages to review compare to your watershed. Use the drop down box to select which gages you 
+                                                           want to review after you have identified them on the map. Gage numbers are listed both in the attribute table 
+                                                           as well as directly above the drop down menu.'),
+                                                        column(2,
+                                                               actionButton('zoomButton',"Zoom to Lat/Long"),#,class='btn-block'),
+                                                               numericInput('lat',"Latitude",value=NA),
+                                                               numericInput('long',"Longitude",value = NA)),
+                                                        
+                                                        column(4,fluidRow(
+                                                          wellPanel(textOutput('gageText'),
+                                                                    helpText('Use the drop down to select up to four gages to review further. You can scroll
+                                                                             through the list or begin to type the gage number you want to keep to see a filtered
+                                                                             list of gages based on your input.'),
+                                                                    selectizeInput('gageList',h5(strong('Gages to Review')),choices=gageInfo@data$GageNo,multiple=T))
+                                                         )),
+                                                        column(4,
+                                                               tableOutput('gageInfoTable'))))),
+                             #bsModal('zoom',"Zoom to Specific Latitude & Longitude",'zoomButton',size='small',
+                            #         numericInput('lat',"Latitude",value=NA),
+                             #        numericInput('long',"Longitude",value = NA),
+                              #       actionButton('submitZoom','Zoom')),
+                             tabPanel("Tab2"),
+                            tabPanel('About',fluidRow(column(10,
+                                                             h5("This tool was created to assist VDEQ  staff in the permit review process."),
+                                                             p("Other stuff."))))
+                            
                   )
                   )
         )
