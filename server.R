f98526c2014e87bf4728ce3e61db47c11d480c16
Emma Jones <evjones@localhost.localdomain>
2017-05-05 16:23:44 -0400

    Name updates on CDF  plot, not sure how to clear plot when changing map parameters

--- server.R
+++ server.R
@@ -234,19 +234,70 @@ shinyServer(function(input, output, session) {
     names(df) <- c('Metal','Subpopulation','n','5%','10%','25%','50%')
     return(df)})
   
+  
+
+  # Subset data on user map click to highlight on CDF curve
+  marker <- reactive({
+    click <- input$weightedMap_marker_click
+    if(is.null(click))
+      return()
+    return(click$id)
+  })
+  
   # Updating dissolved metals cdf plot
-  output$weightedMetalsCDF <- renderPlot({
+  wCDFplot <- reactive({
     if(is.null(metalsCDF_DataSelect()))
       return(NULL)
     m <- max(metalsCDF_DataSelect()$NResp)
     xaxis <- as.character(paste(capwords(tolower(metalsCDF_DataSelect()$Indicator[1])),' (',metalsCDF_DataSelect()$units[1],')'))
-    ggplot(metalsCDF_DataSelect(), aes(x=Value,y=Estimate.P)) + geom_point() + labs(x=xaxis,y="Percentile") +
+    p <- ggplot(metalsCDF_DataSelect(), aes(x=Value,y=Estimate.P)) + geom_point() + labs(x=xaxis,y="Percentile") +
       ggtitle(as.character(paste(capwords(tolower(metalsCDF_DataSelect()$Indicator[1])),'in \n',
                                  metalsCDF_DataSelect()$Subpopulation[1],'\n (n=',m,')'))) + 
       theme(plot.title = element_text(hjust=0.5,face='bold',size=15)) +
       theme(axis.title = element_text(face='bold',size=12))+ 
       geom_ribbon(data=metalsCDF_DataSelect(),aes(ymin=LCB95Pct.P,ymax=UCB95Pct.P),alpha=0.3)
-    })
+    if(is.null(marker())){
+      return(p)
+    }else{
+      xspot <- max(metalsCDF_DataSelect()$Value)*0.7
+      dat <- subset(metalsSites,StationID_Trend %in% as.character(marker()))%>%
+        select_(toupper(input$metalToPlot))
+      cdf <- select(metalsCDF_DataSelect(),Value,Estimate.P)
+      df <- data.frame(Value=dat[[1]],Estimate.P=vlookup(dat[[1]],cdf,2,TRUE))
+      return(p+geom_point(data=df,aes(x=Value,y=Estimate.P),color='orange',size=4)+
+               annotate("text",label=paste(sep= "\n","Percentile Highlighted \nfor StationID:",marker()),
+                        x=xspot,y=10,hjust=0,vjust=0))}
+  })
+  output$weightedMetalsCDF <- renderPlot({wCDFplot()})
+  
+  
+  
+  
+  
+  #output$weightedMetalsCDF <- renderPlot({
+  #  if(is.null(metalsCDF_DataSelect()))
+  #    return(NULL)
+  #  m <- max(metalsCDF_DataSelect()$NResp)
+  #  xaxis <- as.character(paste(capwords(tolower(metalsCDF_DataSelect()$Indicator[1])),' (',metalsCDF_DataSelect()$units[1],')'))
+  #  p <- ggplot(metalsCDF_DataSelect(), aes(x=Value,y=Estimate.P)) + geom_point() + labs(x=xaxis,y="Percentile") +
+  #    ggtitle(as.character(paste(capwords(tolower(metalsCDF_DataSelect()$Indicator[1])),'in \n',
+  #                               metalsCDF_DataSelect()$Subpopulation[1],'\n (n=',m,')'))) + 
+  #    theme(plot.title = element_text(hjust=0.5,face='bold',size=15)) +
+  #    theme(axis.title = element_text(face='bold',size=12))+ 
+  #    geom_ribbon(data=metalsCDF_DataSelect(),aes(ymin=LCB95Pct.P,ymax=UCB95Pct.P),alpha=0.3)
+  #  if(is.null(marker())){
+  #    return(p)
+  #    }else{
+  #      dat <- subset(metalsSites,StationID_Trend %in% as.character(marker()))%>%
+  #        select_(toupper(input$metalToPlot))
+  #      cdf <- select(metalsCDF_DataSelect(),Value,Estimate.P)
+  #      df <- data.frame(Value=dat[[1]],Estimate.P=vlookup(dat[[1]],cdf,2,TRUE))
+  #      return(p+geom_point(data=df,aes(x=Value,y=Estimate.P),color='orange',size=4)
+  #               )
+          
+  #      }
+    
+  #  })
   
   ##---------------------------------------RMARKDOWN SECTION----------------------------------------------
   
@@ -261,10 +312,37 @@ shinyServer(function(input, output, session) {
                         params=params, envir=new.env(parent=globalenv()))})
   ##------------------------------------------------------------------------------------------------------
 
-  
-  
-  
   ## Background Metals UNweighted (all data) Section
+  
+  #output$weightedMap <- renderLeaflet({
+  #  leaflet(metalsSites) %>% addProviderTiles('Thunderforest.Landscape') %>%
+  #    fitBounds(~min(LongitudeDD),~min(LatitudeDD),~max(LongitudeDD),~max(LatitudeDD))%>%
+  #    #fitBounds(metalsSites1@bbox)
+  #    addMouseCoordinates()%>%addHomeButton(extent(metalsSites1), "Virginia")
+  #})
+  
+  # Subset metals CDF data based on user metal and subpopulation #
+  #metalsCDF_DataSelect <- reactive({
+  #  if(input$metalToPlot=="No Metals")
+  #    return(NULL)
+  #  df <- filter(metalsCDF,Indicator==toupper(input$metalToPlot))
+  #  if(input$subpopToPlot=="Virginia"){
+  #    return(filter(df,Subpopulation=="Virginia"))}else{return(filter(df,Subpopulation==input$subpopToPlot))}
+  #})
+  
+  # Subset metals sites based on user metal and subpopulation #
+  #metalsSites_DataSelect <- reactive({
+  #  if(is.null(metalsCDF_DataSelect()))
+  #    return(NULL)
+  #  df <- filter(metalsSites_long,metal==toupper(input$metalToPlot))
+  #  if(input$subpopToPlot=="Virginia")
+  #    return(filter(df,category=='Basin'))
+  #  return(filter(df,Subpopulation==input$subpopToPlot))
+  #})
+  
+  
+  
+  
   
   #output$unweightedMap <- renderMapview({
   #  mapview( Ecoregions, zcol="US_L3NAME")+# popup = popupTable( eco2 , zcol = c("US_L3NAME")))+
